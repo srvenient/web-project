d3a7c701f190b7651ace810abb5684d96514d759
srvenient <srvenient@gmail.com>
2024-09-10 14:33:56 -0500

    refactor(repository): extend StudentRepository with JpaRepository
    - Refactored StudentRepository to extend JpaRepository for CRUD operations
    - Enabled Student entity management with UUID as the identifier

--- src/main/java/co/edu/unimonserrate/webproject/student/domain/StudentRepository.java
+++ src/main/java/co/edu/unimonserrate/webproject/student/domain/StudentRepository.java
@@ -1,7 +1,10 @@
 package co.edu.unimonserrate.webproject.student.domain;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface StudentRepository {
+public interface StudentRepository extends JpaRepository<Student, UUID> {
 }
