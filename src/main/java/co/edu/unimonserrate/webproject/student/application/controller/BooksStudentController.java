ed55258b3211f79fccb685d3ca26891fdc61ef20
yefri <ysrincon@unimonserrate.edu.co>
2024-09-21 15:16:24 -0500

    feat: add endpoint for get all books to the DB

--- src/main/java/co/edu/unimonserrate/webproject/student/application/controller/BooksStudentController.java
+++ src/main/java/co/edu/unimonserrate/webproject/student/application/controller/BooksStudentController.java
@@ -1,14 +1,33 @@
 package co.edu.unimonserrate.webproject.student.application.controller;
 
+import co.edu.unimonserrate.webproject.books.application.BookService;
+import co.edu.unimonserrate.webproject.books.domain.Book;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/api/v1/books")
 public final class BooksStudentController {
-  @RequestMapping
+
+  private final BookService bookService;
+
+  public BooksStudentController(BookService bookService) {
+    this.bookService = bookService;
+  }
+
+  @GetMapping
   public @NotNull String show() {
     return "books";
   }
+
+  @GetMapping("/all")
+  public @NotNull List<Book> getAll() {
+    return new ArrayList<>(this.bookService.getAllBooks());
+  }
+
 }
